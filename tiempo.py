2fd9ccba53b9a9280f4e322393fd53659774cf39
Miguel <unpocodetodokk@gmail.com>
2023-09-25 08:03:58 -0300

    subiendo archivos Benjamin Prieto

---
+++ tiempo.py
@@ -0,0 +1,10 @@
+def segundos_a_minutos(segundos):
+    return segundos /60
+
+def minutos_a_horas(minutos):
+    return minutos /60
+
+if __name__ == "__main__":
+    segundos= 150
+    minutos = segundos_a_minutos(segundos)
+    print(f"{segundos} segundos son equivalentes a {minutos} minutos")
